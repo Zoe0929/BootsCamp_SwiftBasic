342d85e5d339abcee2f930b6f6643ffb87fd6fb9
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-01-22 23:52:26 +0900

    개발하는 정대리 DAY2 : enum

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//swift 의 enum : 케이스 , 경우를 나누는 키워드
+//ex) 학교 - 초, 중, 고
+enum School{
+    case elementary, middle, high
+}
+//let: 상수, var: 변수
+let yourSchool=School.high
+print("yourSchool:\(yourSchool)")
+//print("yourSchool:",yourSchool)
 
-var isDarkMode:Bool=true
+enum Grade : Int{
+    case first=1
+    case second=2
+}
 
-if(isDarkMode==true) {print("다크모드입니다.")}
-else {print("라이트모드입니다.")}
+let yourGrade=Grade.second.rawValue //case의 값에 접근
+print("yourGrade:\(yourGrade)")
 
