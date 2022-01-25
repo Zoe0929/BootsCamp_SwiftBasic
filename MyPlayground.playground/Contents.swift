a63e8d7db73624b8a0cfb11a1074af42cd913091
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-01-25 23:28:39 +0900

    DAY 2: foreach문

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -43,3 +43,20 @@ for i in 0...5 where i % 2 == 0 {
 }//(where은 조건문.)
 
 //0...5 -> 0이상 5이하, 0...<5 0이상 5미만(5 포함 X)
+
+
+//day2 foreach 반복문
+
+var myArray: [Int]=[0,1,2,3,4,5,6,7,8,9,10]
+//콜렉션- 데이터를 모아둔 것.
+//종류 : 배열,셋,딕셔너리
+
+for item in myArray{
+    print("item: \(item)")
+}
+
+for item in myArray where item%2==0{
+    print("짝수:\(item)")
+}
+
+
