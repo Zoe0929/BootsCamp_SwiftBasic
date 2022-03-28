7ae35e0fd3b54bee7e79f4694562e8bdae9f0373
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-03-28 18:55:55 +0900

    new Project

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 지희의 MAC on 2022/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
