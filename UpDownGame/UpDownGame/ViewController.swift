67a8af6b815dc937293e2f31f159df101028ed51
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-04-01 01:32:52 +0900

    IBAction / IBOutlet

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func sliderValueChanged ( sender: UISlider){
+    @IBAction func sliderValueChanged (_ sender: UISlider){
         print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton)
+    {
+        print(slider.value)
     }
 
 }
