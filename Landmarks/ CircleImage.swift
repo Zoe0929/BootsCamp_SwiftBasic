e880deb234c58a32971d9f6d49d5f62471a02c6b
Zoe <Zoe.urssu@gmail.com>
2023-03-16 16:57:39 +0900

    [SwiftUI] 1주차

---
+++ Landmarks/ CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//   CircleImage.swift
+//  Landmarks
+//
+//  Created by 지희의 MAC on 2023/03/16.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 지희의 MAC on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
